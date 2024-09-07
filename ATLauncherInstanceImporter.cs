efac1f1510d8aeb7dc2a38c70e0224233a41abce
adamr <adamr@DESKTOP-7RONQ8P>
2024-09-06 18:20:21 -0700

    Add project files.

---
+++ ATLauncherInstanceImporter.cs
@@ -0,0 +1,87 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ATLauncherInstanceImporter
+{
+    public class ATLauncherInstanceImporter : LibraryPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private ATLauncherInstanceImporterSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("40e56f44-4955-40ec-9bf3-682c4007e55b");
+
+        // Change to something more appropriate
+        public override string Name => "ATLauncher";
+
+        // Implementing Client adds ability to open it via special menu in playnite.
+        public override LibraryClient Client { get; } = new ATLauncherInstanceImporterClient();
+
+        public ATLauncherInstanceImporter(IPlayniteAPI api) : base(api)
+        {
+            settings = new ATLauncherInstanceImporterSettingsViewModel(this);
+            Properties = new LibraryPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        {
+            // Return list of user's games.
+            return new List<GameMetadata>()
+            {
+                new GameMetadata()
+                {
+                    Name = "Notepad",
+                    GameId = "notepad",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "notepad.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"c:\Windows\notepad.exe")
+                },
+                new GameMetadata()
+                {
+                    Name = "Calculator",
+                    GameId = "calc",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "calc.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"https://playnite.link/applogo.png"),
+                    BackgroundImage = new MetadataFile(@"https://playnite.link/applogo.png")
+                }
+            };
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new ATLauncherInstanceImporterSettingsView();
+        }
+    }
+}
---
+++ ATLauncherInstanceImporterSettings.cs
@@ -0,0 +1,100 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ATLauncherInstanceImporter
+{
+    public class ATLauncherInstanceImporterSettings : ObservableObject
+    {
+        private string aTLauncherLoc = string.Empty;
+        private bool showATLauncherConsole = false;
+        private bool closeATLOnLaunch = true;
+
+        public string ATLauncherLoc { get => aTLauncherLoc; set => SetValue(ref aTLauncherLoc, value); }
+        public bool ShowATLauncherConsole { get => showATLauncherConsole; set => SetValue(ref showATLauncherConsole, value); }
+        public bool CloseATLOnLaunch { get => closeATLOnLaunch; set => SetValue(ref closeATLOnLaunch, value); }
+        // Playnite serializes settings object to a JSON object and saves it as text file.
+        // If you want to exclude some property from being saved then use `JsonDontSerialize` ignore attribute.
+        //[DontSerialize]
+        //public bool OptionThatWontBeSaved { get => optionThatWontBeSaved; set => SetValue(ref optionThatWontBeSaved, value); }
+    }
+
+    public class ATLauncherInstanceImporterSettingsViewModel : ObservableObject, ISettings
+    {
+        private readonly ATLauncherInstanceImporter plugin;
+        private ATLauncherInstanceImporterSettings editingClone { get; set; }
+
+        private ATLauncherInstanceImporterSettings settings;
+        public ATLauncherInstanceImporterSettings Settings
+        {
+            get => settings;
+            set
+            {
+                settings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ATLauncherInstanceImporterSettingsViewModel(ATLauncherInstanceImporter plugin)
+        {
+            // Injecting your plugin instance is required for Save/Load method because Playnite saves data to a location based on what plugin requested the operation.
+            this.plugin = plugin;
+
+            // Load saved settings.
+            var savedSettings = plugin.LoadPluginSettings<ATLauncherInstanceImporterSettings>();
+
+            // LoadPluginSettings returns null if no saved data is available.
+            if (savedSettings != null)
+            {
+                Settings = savedSettings;
+            }
+            else
+            {
+                Settings = new ATLauncherInstanceImporterSettings();
+            }
+        }
+
+        public void BeginEdit()
+        {
+            // Code executed when settings view is opened and user starts editing values.
+            editingClone = Serialization.GetClone(Settings);
+        }
+
+        public void CancelEdit()
+        {
+            // Code executed when user decides to cancel any changes made since BeginEdit was called.
+            // This method should revert any changes made to Option1 and Option2.
+            Settings = editingClone;
+        }
+
+        public void EndEdit()
+        {
+            // Code executed when user decides to confirm changes made since BeginEdit was called.
+            // This method should save settings made to Option1 and Option2.
+            plugin.SavePluginSettings(Settings);
+        }
+
+        public bool VerifySettings(out List<string> errors)
+        {
+            ILogger logger = LogManager.GetLogger();
+            // Code execute when user decides to confirm changes made since BeginEdit was called.
+            // Executed before EndEdit is called and EndEdit is not called if false is returned.
+            // List of errors is presented to user if verification fails.
+            errors = new List<string>();
+
+            if (File.Exists(Settings.ATLauncherLoc + "\\ATLauncher.exe"))
+            {
+                logger.Debug("ATLauncher executable found at given directory");
+                return true;
+            }
+            logger.Debug(Settings.ATLauncherLoc + "ATLauncher.exe does not contain ATLauncher.exe");
+            errors.Add("ATLauncher executable not found at given location");
+            return false;
+        }
+    }
+}
